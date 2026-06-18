2bff24b26bd300768e2a254a1107a375a456dd26
berokwiri150-26 <berokwiri150@gmail.com>
2026-06-18 22:35:33 +0300

    Commits

--- lib/book.py
+++ lib/book.py
@@ -7,6 +7,21 @@ class Book:
         self.current_page = 1
 
 
+    @property
+    def page_count(self):
+        return self._page_count
+    
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, int):
+            self._page_count = value
+        else: 
+            print("page_count must be an integer.")
+
+    def turn_page(self):
+        print(f"Turned page to {self.current_page}.")
+
+
     def turn_page(self):
         if self.current_page < self.page_count:
             self.current_page += 1
--- lib/coffee.py
+++ lib/coffee.py
@@ -6,6 +6,19 @@ class Coffee:
         self.price = price
         self.tips = 0
 
+    @property
+    def size (self):
+        return self._size
+    
+    @size.setter
+    def size(self, value):
+        if value in ["Small", "Medium", "Large"]:
+            self._size = value
+        else:
+            print("Size must be 'Small', 'Medium', or 'Large'.")
+
+        
+
     def tip(self, amount):
         self.tips += amount
         print(f"Added a tip of {amount}. Total tips:{self.tips}.")
